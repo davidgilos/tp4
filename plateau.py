51178ea6507e34c95aa2851a1f004ad4b4f13579
Jeremy Rea <rea.jeremy@gmail.com>
2013-11-07 18:58:49 -0500

    Création classe Plateau

--- plateau.py
+++ plateau.py
@@ -8,11 +8,14 @@
 Module qui s'occupe de dessiner l'échequier.
 """
 
-def draw():
-	for i in range (0, 8):
-		print(8-i, " ", end="")
-		for j in range (0, 8):
-			print("[ ] ", end="")
-		print("\n")
-	print("    1   2   3   4   5   6   7   8")
-draw()
+class Plateau:
+	def affichage(self):
+		for i in range (0, 8):
+			print(8-i, " ", end="")  # Imprime le numéro de la ligne 
+			for j in range (0, 8):
+				print("[ ] ", end="")  # Imprime les éléments d'une ligne
+			print("\n")
+		print("    1   2   3   4   5   6   7   8")  # Imprime le numéro des colones
+
+tableau = Plateau()
+tableau.affichage()
