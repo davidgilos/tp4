0eb029af941e8c6b69d5f7e70659142e312b136a
Jeremy Rea <rea.jeremy@gmail.com>
2013-11-12 12:22:28 -0500

    changements

--- plateau.py
+++ plateau.py
@@ -8,11 +8,18 @@
 Module qui s'occupe de dessiner l'échequier.
 """
 
+from tour import Tour
+
 class Plateau:   
-	def __init__(self):
-	
+    def __init__(self):
+		
+    #    Initialise le plateau avec un damier vide
+    #    self.damier = {(0,0):Tour(0,0,0), (0,7):Tour(0,7,0), (7,0):Tour(7,0,1), (7,7):Tour(7,7,1)}
+    
 		# Le nom des pièces dans le dictionnaire sera changé pour la classe de la pièce
-		self.pieces_noir = {
+		
+			
+        self.pieces_noir = {
 						        (7,0): "Tour",
 								(7,1): "Cavalier",
 								(7,2): "Fou",
@@ -30,7 +37,7 @@ class Plateau:
 								(6,6): "Pion",
 								(6,7): "Pion"
 						   }
-		self.pieces_blanc = {
+        self.pieces_blanc = {
 							    (0,0): "Tour",
 								(0,1): "Cavalier",
 								(0,2): "Fou",
@@ -48,12 +55,22 @@ class Plateau:
 								(1,6): "Pion",
 								(1,7): "Pion"
 							}
+							
+
+    def getPiece(self,line,col):
+        #Retourne la piece à la position (line,col) ou None sinon
+        return self.damier.get((line,col))
+#  Identique a :        
+#        if (line,col) in self.damier.keys() :
+#            return self.damier[(line,col)]
+#        else:
+#            return None
 
 
-	def affichage(self):
+    def affichage(self):
 
 		# Représentation graphique des pièces
-		PIECE_BLANC = {
+        PIECE_BLANC = {
 	   		        	"Roi": 	    "♚",
 						"Dame":     "♛",
 						"Tour":     "♜",
@@ -61,7 +78,7 @@ class Plateau:
 						"Cavalier": "♞",
 						"Pion":     "♟"
 					 }  
-		PIECE_NOIR = {
+        PIECE_NOIR = {
 						"Roi":      "♔",
 						"Dame":     "♕",
 						"Tour":     "♖",
@@ -70,17 +87,17 @@ class Plateau:
 						"Pion":     "♙"
 				 	  }
 
-		for x in range (0, 8):
-			print(8-x, " ", end="")  # Imprime le numéro de la ligne 
-			for y in range (0, 8):
-				if (x,y) in self.pieces_blanc:  # Vérifie si la case contient une pièce blanche
-					print("[" + PIECE_BLANC[self.pieces_blanc[x,y]] + "] ", end="")  # Imprime la pièce.  Le + assure qu'il n'y a pas d'espace d'inserré des deux côtés de la figure
-				elif (x,y) in self.pieces_noir:  # Vérifie si la case contient une pièce noire
-					print("[" + PIECE_NOIR[self.pieces_noir[x,y]] + "] ", end="")  # Imprime la pièce		
-				else:
-					print("[ ] ", end="")  # Aucune pièce présente, case vide
-			print("\n")
-		print("    1   2   3   4   5   6   7   8")  # Imprime le numéro des colones
+        for x in range (0, 8):
+            print(8-x, " ", end="")  # Imprime le numéro de la ligne 
+            for y in range (0, 8):
+                if (x,y) in self.pieces_blanc:  # Vérifie si la case contient une pièce blanche
+                    print("[" + PIECE_BLANC[self.pieces_blanc[x,y]] + "] ", end="")  # Imprime la pièce.  Le + assure qu'il n'y a pas d'espace d'inserré des deux côtés de la figure
+                elif (x,y) in self.pieces_noir:  # Vérifie si la case contient une pièce noire
+                    print("[" + PIECE_NOIR[self.pieces_noir[x,y]] + "] ", end="")  # Imprime la pièce		
+                else:
+                    print("[ ] ", end="")  # Aucune pièce présente, case vide
+            print("\n")
+        print("    1   2   3   4   5   6   7   8")  # Imprime le numéro des colones
 
 tableau = Plateau()
 tableau.affichage()
